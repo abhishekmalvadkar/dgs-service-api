8d370175e9de10e97ba169bbe38cf704417791d4
Abhishek Malvadkar <abhi@dev.com>
2025-07-06 12:07:02 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/dgs/DgsServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.dgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgsServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgsServiceApiApplication.class, args);
+	}
+
+}
